57dce93db0115076b8451d2f69c8d392af3189d0
kangjh3232-png <kangjh3232@gmail.com>
2026-04-04 20:18:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dangatop-v2';
+const CACHE = 'dangatop-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
