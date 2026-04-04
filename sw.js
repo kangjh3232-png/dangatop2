306a9e658f601c9d9496ad01b5f4ac2d55ade3df
kangjh3232-png <kangjh3232@gmail.com>
2026-04-04 22:25:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE = 'dangatop-v5';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// 서비스워커 비활성화 - 캐시 없이 항상 네트워크에서 직접 로드
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(ks =>
-      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
-
-// 모든 요청을 네트워크 우선으로 처리 (캐시 문제 방지)
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+// fetch 이벤트 핸들러 없음 = 캐시 사용 안 함, 항상 네트워크
