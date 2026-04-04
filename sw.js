5d54348257d0344f7c52afdf5fe6e1cdc11cefce
kangjh3232-png <kangjh3232@gmail.com>
2026-04-04 22:16:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE = 'dangatop-v4';
-const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'dangatop-v5';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))
-  );
 });
 
 self.addEventListener('activate', e => {
@@ -16,23 +12,15 @@ self.addEventListener('activate', e => {
   );
 });
 
+// 모든 요청을 네트워크 우선으로 처리 (캐시 문제 방지)
 self.addEventListener('fetch', e => {
-  // index.html과 sw.js는 항상 네트워크 우선
-  const url = new URL(e.request.url);
-  if(url.pathname.endsWith('index.html') || url.pathname.endsWith('sw.js') || url.pathname.endsWith('/')) {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-          return res;
-        })
-        .catch(() => caches.match(e.request))
-    );
-    return;
-  }
-  // 나머지는 캐시 우선
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
